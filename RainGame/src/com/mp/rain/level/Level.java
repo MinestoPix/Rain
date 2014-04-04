2275aa5353698d09925ba3f7011901b7c3d5ba06
MinestoPix <zan.stopar@gmail.com>
2014-04-05 00:58:42 +0200

    added levels, I should go to sleep

---
+++ RainGame/src/com/mp/rain/level/Level.java
@@ -0,0 +1,46 @@
+package com.mp.rain.level;
+
+import com.mp.rain.graphics.Screen;
+
+public class Level {
+	
+	private int width, height;
+	private int[] tiles;
+	
+	public Level(int width, int height) {
+		
+		this.width = width;
+		this.height = height;
+		tiles = new int[width * height];
+		
+		generateLevel();
+		
+	}
+	
+	public Level(String path) {
+		
+		loadLevel(path);
+		
+	}
+	
+	private void generateLevel() {
+		
+	}
+	
+	private void loadLevel(String path) {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	private void time() {
+		
+	}
+	
+	public void render(int xScroll, int yScroll, Screen screen) {
+		
+	}
+	
+}
