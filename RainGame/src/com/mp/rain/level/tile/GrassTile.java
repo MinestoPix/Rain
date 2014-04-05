127367f210ffab6e56b951b260496daf1d632a9e
MinestoPix <zan.stopar@gmail.com>
2014-04-05 02:12:51 +0200

    sleeping already..

---
+++ RainGame/src/com/mp/rain/level/tile/GrassTile.java
@@ -0,0 +1,21 @@
+package com.mp.rain.level.tile;
+
+import com.mp.rain.graphics.Screen;
+import com.mp.rain.graphics.Sprite;
+
+public class GrassTile extends Tile {
+	
+	public GrassTile(Sprite sprite) {
+		
+		super(sprite);
+		
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		
+		// TODO Rendering
+		super.render(x, y, screen);
+		
+	}
+	
+}
